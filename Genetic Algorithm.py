48a2ba30401e9940aab84ff7aff4dfea947fdba0
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-03-05 01:49:15 +0600

    new

--- Genetic Algorithm.py
+++ Genetic Algorithm.py
@@ -1,68 +1,35 @@
 import random
-import numpy as np
 
-class GeneticMutation:
-    def __init__(self, population_size, num_generations, mutation_rate):
-        self.population_size = population_size
-        self.num_generations = num_generations
-        self.mutation_rate = mutation_rate
-        self.population = []
+# Parameters
+target = "1010101010"
+pop_size = 10
+mut_rate = 0.1
+gens = 50
 
-    def generate_initial_population(self):
-        for _ in range(self.population_size):
-            individual = [random.randint(0, 1) for _ in range(10)]  # 10-bit binary string
-            self.population.append(individual)
+# Fitness function: number of matching bits
+def fitness(ind):
+    return sum(a==b for a,b in zip(ind,target))
 
-    def fitness(self, individual):
-        # Simple fitness function: sum of the bits
-        return sum(individual)
+# Random individual
+def rand_ind():
+    return ''.join(random.choice('01') for _ in target)
 
-    def selection(self):
-        fitnesses = [self.fitness(individual) for individual in self.population]
-        selected_individuals = []
-        for _ in range(self.population_size):
-            r = random.random()
-            cumulative_sum = 0
-            for i, individual in enumerate(self.population):
-                cumulative_sum += fitnesses[i]
-                if r <= cumulative_sum / sum(fitnesses):
-                    selected_individuals.append(individual)
-                    break
-        return selected_individuals
+# Crossover
+def crossover(p1,p2):
+    i = random.randint(1,len(p1)-1)
+    return p1[:i]+p2[i:]
 
-    def crossover(self, parents):
-        child = []
-        for i in range(len(parents[0])):
-            if random.random() > 0.5:
-                child.append(parents[0][i])
-            else:
-                child.append(parents[1][i])
-        return child
+# Mutation
+def mutate(ind):
+    return ''.join(b if random.random()>mut_rate else random.choice('01') for b in ind)
 
-    def mutation(self, individual):
-        if random.random() < self.mutation_rate:
-            for i in range(len(individual)):
-                if random.random() < 0.5:
-                    individual[i] = 1 - individual[i]
-        return individual
+# Initial population
+pop = [rand_ind() for _ in range(pop_size)]
 
-    def evolve(self):
-        selected_individuals = self.selection()
-        new_population = []
-        for _ in range(self.population_size):
-            parents = random.sample(selected_individuals, 2)
-            child = self.crossover(parents)
-            child = self.mutation(child)
-            new_population.append(child)
-        self.population = new_population
+for g in range(gens):
+    pop = sorted(pop,key=fitness,reverse=True)
+    if fitness(pop[0])==len(target):
+        break
+    pop = [mutate(crossover(random.choice(pop[:5]),random.choice(pop[:5]))) for _ in range(pop_size)]
 
-    def run(self):
-        self.generate_initial_population()
-        for _ in range(self.num_generations):
-            self.evolve()
-        return self.population
-
-# Example usage
-algorithm = GeneticMutation(population_size=100, num_generations=100, mutation_rate=0.01)
-population = algorithm.run()
-print(population)
+print("Best:",pop[0],"Fitness:",fitness(pop[0]))
